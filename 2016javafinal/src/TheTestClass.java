50db0cbd502280e5e988a0de9720f010d831a4b6
djohnson7823 <djohnson7823@001LAB153010815.neisd.ad>
2016-05-16 10:20:43 -0500

    htedz

---
+++ 2016javafinal/src/TheTestClass.java
@@ -0,0 +1,12 @@
+
+public class TheTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		pasurswurd x = new pasurswurd();
+		x.setpassword();
+		x.check();
+		x.info();
+	}
+
+}
