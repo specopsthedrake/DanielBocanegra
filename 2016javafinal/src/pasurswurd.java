3dd7f3bdb6bc29326da42f42def9241b503d6df0
djohnson7823 <djohnson7823@001LAB153010815.neisd.ad>
2016-05-17 10:07:31 -0500

    hytr

--- 2016javafinal/src/pasurswurd.java
+++ 2016javafinal/src/pasurswurd.java
@@ -7,6 +7,7 @@ public class pasurswurd {
 
 	private ArrayList<String> passcodes = new ArrayList();
 	private ArrayList<String> messages = new ArrayList();
+	private ArrayList<String> files = new ArrayList();
 
 	public void setpassword() {
 		int passs = Integer.parseInt(JOptionPane.showInputDialog("How many Passwords?"));
@@ -15,22 +16,27 @@ public class pasurswurd {
 			String y;
 
 			passcodes.add(JOptionPane.showInputDialog("What's the password?"));
-			messages.add(JOptionPane.showInputDialog("What's the messag for this password?"));
+			messages.add(JOptionPane.showInputDialog("What's the message for this password?"));
 		}
 	}
 
 	public int check() {
 		for (int i = 0; i < passcodes.size(); i++)
 
+			// if they get the password wrong
 			if (!JOptionPane.showInputDialog("Enter the password number " + (i + 1)).equals(passcodes.get(i))) {
+				// exit and return how far they got
 				return i - 1;
+				// if they got it right
+			} else
+			// System.out.println("files are the over ride");
+			{
+				// What's this do
+				JOptionPane.showMessageDialog(null, messages.get(i));
+
 			}
-
 		return passcodes.size();
-
-	}
-
-	public void info() {
+		// public void info() {
 		// TODO Auto-generated method stub
 
 	}
