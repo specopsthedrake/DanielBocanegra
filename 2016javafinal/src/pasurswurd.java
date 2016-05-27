ca1649c0a2111d29e8453e8accfcc2566803e819
djohnson7823 <djohnson7823@001LAB153010815.neisd.ad>
2016-05-27 10:58:40 -0500

    done but can be improved

--- 2016javafinal/src/pasurswurd.java
+++ 2016javafinal/src/pasurswurd.java
@@ -1,43 +1,86 @@
 
+import java.awt.HeadlessException;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
 public class pasurswurd {
 
-	private ArrayList<String> passcodes = new ArrayList();
-	private ArrayList<String> messages = new ArrayList();
+	private String master;
+	private ArrayList<String> passcodes = new ArrayList<String>();
+	private ArrayList<String> messages = new ArrayList<String>();
 	private ArrayList<String> files = new ArrayList();
 
-	public void setpassword() {
+	// main part for secuity thourgh hashes. To improve move password hashes in
+	// a .dat file as well a messages
+	public String info(String message) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		// sets diffulty of hashes
+		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		byte[] inputBytes = digest.digest(message.getBytes("UTF-8"));
+
+		// ask poore
+		byte[] hashBytes = digest.digest(inputBytes);
+		return convertByteArrayToHexString(hashBytes);
+
+	}
+
+	// connects to the changing passwords and messages
+	private static String convertByteArrayToHexString(byte[] arrayBytes) {
+		StringBuffer stringBuffer = new StringBuffer();
+		for (int i = 0; i < arrayBytes.length; i++) {
+			stringBuffer.append(Integer.toString((arrayBytes[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		return stringBuffer.toString();
+	}
+	// TODO Auto-generated method stub
+	/*
+	 * public static String generateMD5(String message) throws
+	 * HashGenerationException { return hashString(message, "MD5"); }
+	 * 
+	 * public static String generateSHA1(String message) throws
+	 * HashGenerationException { return hashString(message, "SHA-1"); }
+	 * 
+	 * public static String generateSHA256(String message) throws
+	 * HashGenerationException { return hashString(message, "SHA-256"); }
+	 */
+
+	// creates the changing passwords and messages
+	public void setpassword() throws NoSuchAlgorithmException, UnsupportedEncodingException {
 		int passs = Integer.parseInt(JOptionPane.showInputDialog("How many Passwords?"));
 
 		for (int i = 0; i < passs; i++) {
-			String y;
-
-			passcodes.add(JOptionPane.showInputDialog("What's the password?"));
+			passcodes.add(info(JOptionPane.showInputDialog("What's the password?")));
+			System.out.println(info(passcodes.get(i).toString()));
 			messages.add(JOptionPane.showInputDialog("What's the message for this password?"));
+
 		}
+		master = "four";
+
 	}
 
-	public int check() {
+	public int check() throws HeadlessException, NoSuchAlgorithmException, UnsupportedEncodingException {
+		// return all the messages
+		// } else
+
 		for (int i = 0; i < passcodes.size(); i++)
 
 			// if they get the password wrong
-			if (!JOptionPane.showInputDialog("Enter the password number " + (i + 1)).equals(passcodes.get(i))) {
+
+			if (!info(JOptionPane.showInputDialog("Enter the password number " + (i + 1))).equals(passcodes.get(i))) {
 				// exit and return how far they got
 				return i - 1;
 				// if they got it right
 			} else
-			// System.out.println("files are the over ride");
+
 			{
 				// What's this do
 				JOptionPane.showMessageDialog(null, messages.get(i));
 
 			}
 		return passcodes.size();
-		// public void info() {
-		// TODO Auto-generated method stub
 
 	}
 
